0ea344e9dc0fc6af62d6b105a1407e93d7a9ee5a
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-04-13 10:17:00 +0200

    fix: unterminated string for default codes

--- components/kin.provider.tsx
+++ components/kin.provider.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 // KinContext.tsx
 
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode } from "react";
 
 interface KinContextProps {
   children: ReactNode;
@@ -15,7 +15,9 @@ interface KinContextValue {
 const KinContext = createContext<KinContextValue | undefined>(undefined);
 
 function KinContextProvider({ children }: KinContextProps) {
-  const [editorValue, setEditorValue] = useState<string>("tangaza_amakuru("Muraho ", injiza_amakuru("Izina ryawe: "), "!")");
+  const [editorValue, setEditorValue] = useState<string>(
+    `tangaza_amakuru("Muraho ", injiza_amakuru("Izina ryawe: "), "!")`
+  );
 
   const updateEditorValue = (newEditorValue: string) => {
     setEditorValue(newEditorValue);
