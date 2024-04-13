a423edfd80d47bd8b34a909762c4dff180eb353e
Murangwa Pacifique <96472682+pacifiquem@users.noreply.github.com>
2024-04-13 09:53:15 +0200

    refactor: set the initial editor value

--- components/kin.provider.tsx
+++ components/kin.provider.tsx
@@ -15,7 +15,7 @@ interface KinContextValue {
 const KinContext = createContext<KinContextValue | undefined>(undefined);
 
 function KinContextProvider({ children }: KinContextProps) {
-  const [editorValue, setEditorValue] = useState<string>("Initial Editor Value");
+  const [editorValue, setEditorValue] = useState<string>("tangaza_amakuru("Muraho ", injiza_amakuru("Izina ryawe: "), "!")");
 
   const updateEditorValue = (newEditorValue: string) => {
     setEditorValue(newEditorValue);
