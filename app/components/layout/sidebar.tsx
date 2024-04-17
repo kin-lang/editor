10c39a85149dbae6e72835bbf85c8d00b1005c0a
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-04-17 11:55:37 +0200

    Revert "fix: container errors"
    This reverts commit bea755b90b869128b30b85418bba75ee2f9e268d.

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -1,48 +1,47 @@
-"use client";
-import { samplePrograms } from "@/app/utils/data";
-import { Program } from "@/app/utils/types";
-import { KinContext } from "@/components/kin.provider";
+"use client"
+import { samplePrograms } from "@/app/utils/data"
+import { Program } from "@/app/utils/types"
+import { KinContext } from "@/components/kin.provider"
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "@/components/ui/select"
 
-import { useContext, useState } from "react";
+import {useContext, useState } from "react"
 
 const SideBar = () => {
-  const [selected, setSelected] = useState<Program | undefined>(undefined);
 
-  const context = useContext(KinContext);
+  const [selected, setSelected] = useState<Program | undefined>(undefined)
 
-  const { editorValue, updateEditorValue } = context!;
+  const context = useContext(KinContext)
+
+  const {editorValue, updateEditorValue} = context!
 
   const changeSample = (value: string) => {
-    const program = samplePrograms.find((program) => program.name === value);
-    setSelected(program);
-    updateEditorValue(program?.code ?? "");
-  };
+    const program = samplePrograms.find((program) => program.name === value)
+    setSelected(program)
+    updateEditorValue(program?.code ?? "")
+  }
 
   return (
     <nav className="p-4">
       <div className="mt-4">
-        <Select onValueChange={(e) => changeSample(e)}>
+        <Select onValueChange={(e)=>changeSample(e)}>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Sample programs" />
           </SelectTrigger>
-          <SelectContent>
+          <SelectContent >
             {samplePrograms.map((program, index) => (
-              <SelectItem key={index} value={program.name}>
-                {program.name}
-              </SelectItem>
+              <SelectItem key={index} value={program.name}>{program.name}</SelectItem>
             ))}
           </SelectContent>
         </Select>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default SideBar;
+export default SideBar
--- app/utils/types/index.ts
+++ app/utils/types/index.ts
@@ -1,18 +1,18 @@
-export interface EditorOptions {
-  height: string;
-  width: string;
-  defaultLanguage: string;
-  theme: string;
+export interface EditorOptions{
+    height: string
+    width: string
+    defaultLanguage: string
+    theme: string,
 }
 
-export interface Program {
-  name: string;
-  code: string;
+export interface Program{
+    name: string
+    code: string
 }
 
 export interface KinContextData {
-  editorValue: string;
-  theme: "vs-dark" | "light";
+    editorValue: string;
+    theme: "vs-dark" | "light";
 }
 
-export type KinTheme = "vs-dark" | "light";
+export type KinTheme = "vs-dark" | "light"
