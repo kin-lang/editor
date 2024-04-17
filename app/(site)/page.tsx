bea755b90b869128b30b85418bba75ee2f9e268d
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-04-17 11:42:09 +0200

    fix: container errors

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useContext, useEffect } from "react";
 import KinEditor from "../components/common/editor";
@@ -9,47 +9,58 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
+} from "@/components/ui/resizable";
 import { KinContext } from "@/components/kin.provider";
 import { Button } from "@/components/ui/button";
 import { Play } from "lucide-react";
 
-const defaultOptions:EditorOptions = {
+const defaultOptions: EditorOptions = {
   height: "100%",
   width: "100%",
   defaultLanguage: "javascript",
-  theme: "vs-dark"
-}
+  theme: "vs-dark",
+};
 
 export default function Home() {
+  const context = useContext(KinContext);
 
-  const context = useContext(KinContext)
-
-  const {editorValue} = context!
+  const { editorValue } = context!;
 
   return (
     <main className="h-screen overflow-hidden">
       <NavBar />
-      <ResizablePanelGroup className="bg-black/10 h-[calc(100vh-100px)]" direction="horizontal">
+      <ResizablePanelGroup
+        className="bg-black/10 h-[calc(100vh-100px)]"
+        direction="horizontal"
+      >
         <ResizablePanel defaultSize={20} className="">
           <SideBar />
         </ResizablePanel>
         <ResizableHandle withHandle className="bg-white/70" />
-        <ResizablePanel  defaultSize={80}>
+        <ResizablePanel defaultSize={80}>
           <ResizablePanelGroup direction="vertical">
             <ResizablePanel defaultSize={60} className="">
               <KinEditor {...defaultOptions} defaultValue={editorValue} />
             </ResizablePanel>
             <ResizableHandle withHandle />
             <ResizablePanel defaultSize={40}>
-              <div className="px-4 mt-3 pb-4">
-                <Button className="float-right flex items-center gap-2">
-                  <Play className="h-4 w-4"/>
+              <div className=" flex flex-row gap-8 px-4 mt-3 pb-4 float-right">
+                <Button className="float-left flex items-center gap-2">
+                  <Play className="h-4 w-4" />
                   Run
+                </Button>
+                <Button
+                  className="float-right flex items-center gap-2"
+                  variant="secondary"
+                >
+                  <Play className="h-4 w-4" />
+                  Clear
                 </Button>
               </div>
               <div className="p-4">
-                <span className="text-sm font-extralight">the output will be shown here</span>
+                <span className="text-sm font-extralight">
+                  the output will be shown here
+                </span>
               </div>
             </ResizablePanel>
           </ResizablePanelGroup>
